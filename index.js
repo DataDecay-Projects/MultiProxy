9ba2d826e54159a363b2155655793504a9f2ada7
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-05-09 14:03:33 -0400

    Update index.js
    Signed-off-by: Samuel Wall <149903180+DataDecay@users.noreply.github.com>

--- index.js
+++ index.js
@@ -258,3 +258,24 @@ saveTileButton.addEventListener('click', () => {
 });
 
 loadQuickTiles();
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+});
+
+
